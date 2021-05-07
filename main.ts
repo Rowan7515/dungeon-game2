b4df1440d5aa216a35da9b9b182b468f54620486
Rowan7515 <74774804+Rowan7515@users.noreply.github.com>
2021-05-07 13:00:03 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,55 +3,6 @@ namespace SpriteKind {
     export const Bar = SpriteKind.create()
     export const Bad_projectile = SpriteKind.create()
 }
-function Set_up () {
-    info.setLife(20)
-    Boss_life = 100
-    Player_1 = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    Boss_life_bar = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Bar)
-    controller.moveSprite(Player_1)
-    controller.moveSprite(Boss_life_bar)
-    tiles.setTilemap(tilemap`level1`)
-    scene.cameraFollowSprite(Player_1)
-    tiles.placeOnTile(Player_1, tiles.getTileLocation(8, 15))
-    Boss_life_bar.setPosition(Player_1.x, Player_1.y)
-    Boss_life_bar.x += 60
-    Boss_life_bar.y += -50
-    Boss_life_bar.setStayInScreen(true)
-}
 function Boss_attack_2 () {
     Attack_happening = 1
     Boss_1.say("2", 200)
@@ -229,14 +180,59 @@ let Player_shot: Sprite = null
 let Player_y_for_aim = 0
 let Player_x_for_aim = 0
 let Attack_happening = 0
-let Boss_life_bar: Sprite = null
-let Boss_life = 0
 let Boss_1: Sprite = null
 let Player_1: Sprite = null
-Set_up()
+info.setLife(20)
+let Boss_life = 100
+Player_1 = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let Boss_life_bar = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Bar)
+controller.moveSprite(Player_1)
+controller.moveSprite(Boss_life_bar)
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(Player_1)
+tiles.placeOnTile(Player_1, tiles.getTileLocation(8, 15))
+Boss_life_bar.setPosition(Player_1.x, Player_1.y)
+Boss_life_bar.x += 60
+Boss_life_bar.y += -50
+Boss_life_bar.setStayInScreen(true)
 while (!(Player_1.tileKindAt(TileDirection.Center, sprites.dungeon.collectibleInsignia))) {
     pause(10)
 }
+let Boss_exists = 1
 scene.cameraShake(8, 3000)
 pause(3000)
 Boss_1 = sprites.create(img`
